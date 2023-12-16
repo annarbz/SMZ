1417e60b4766c99cd8cc33c38501e9d32f3deb15
annarbz <103678481+annarbz@users.noreply.github.com>
2023-12-16 11:56:25 +0300

    Add files via upload

--- lab1_test.py
+++ lab1_test.py
@@ -1,52 +1,43 @@
-import unittest
-import numpy as np
-from lab1 import convolution2D  
+import torch
+import torch.nn.functional as F
+from lab1 import convolution2D
 
-class TestConvolution2D(unittest.TestCase):
-    def test_convolution(self):
-        # Тестирование базовой свертки без смещения (bias)
-        input_tensor = np.random.rand(2, 3, 4, 4)  # Размеры: Batch size: 2, In channels: 3, Height: 4, Width: 4
-        weight = np.random.rand(4, 3, 3, 3)  # Размеры: Out channels: 4, In channels/groups: 3, Kernel size: 3x3
+def test_custom_conv2d_1():
+    # Генерация случайных входных данных и фильтров
+    input_tensor = torch.randint(0, 6, (2, 3, 4, 4)).float()
+    filters = torch.randint(0, 6, (2, 3, 2, 2)).float()
 
-        output_tensor = convolution2D(input_tensor, weight)
+    # Выполнение свертки с использованием PyTorch
+    conv_torch = F.conv2d(input_tensor, filters)
 
-        # Утверждения для проверки корректности output_tensor
-        self.assertEqual(output_tensor.shape, (2, 4, 2, 2))  # Пример размеров выходного тензора
-        
+    # Выполнение свертки с использованием моей реализации
+    conv_custom = convolution2D(input_tensor.numpy(), filters.numpy())
 
-    def test_convolution_with_bias(self):
-        # Тестирование свертки с использованием смещения (bias)
-        input_tensor = np.random.rand(2, 3, 4, 4)
-        weight = np.random.rand(4, 3, 3, 3)
-        bias = np.random.rand(4)
+    # Преобразование массива numpy в тензор torch
+    conv_custom = torch.tensor(conv_custom)
 
-        output_tensor = convolution2D(input_tensor, weight, bias)
+    # Сравнение результатов
+    assert torch.allclose(conv_torch, conv_custom)
 
-        # Утверждения для проверки корректности output_tensor
-        self.assertEqual(output_tensor.shape, (2, 4, 2, 2))  # Пример размеров выходного тензора
-        
+def test_custom_conv2d_2():
+    # Генерация случайных входных данных и фильтров
+    input_tensor = torch.randint(0, 6, (2, 3, 4, 4)).float()
+    filters = torch.randint(0, 6, (2, 3, 2, 2)).float()
+    
+    # Выполнение свертки с использованием PyTorch с учетом смещения (bias)
+    bias = torch.randint(0, 6, (2,)).float()
+    conv_torch = F.conv2d(input_tensor, filters, bias=bias)
 
-    def test_convolution_with_stride(self):
-        # Тестирование свертки с использованием заданного шага (stride)
-        input_tensor = np.random.rand(2, 3, 5, 5)
-        weight = np.random.rand(4, 3, 3, 3)
+    # Выполнение свертки с использованием моей реализации с учетом смещения (bias)
+    conv_custom = convolution2D(input_tensor.numpy(), filters.numpy(), bias=bias.numpy())
 
-        output_tensor = convolution2D(input_tensor, weight, stride=2)
+    # Преобразование массива numpy в тензор torch
+    conv_custom = torch.tensor(conv_custom)
 
-        # Утверждения для проверки корректности output_tensor
-        self.assertEqual(output_tensor.shape, (2, 4, 2, 2))  # Пример размеров выходного тензора
-        
+    # Сравнение результатов
+    assert torch.allclose(conv_torch, conv_custom)
 
-    def test_convolution_with_padding(self):
-        # Тестирование свертки с использованием заданного отступа (padding)
-        input_tensor = np.random.rand(2, 3, 4, 4)
-        weight = np.random.rand(4, 3, 3, 3)
-
-        output_tensor = convolution2D(input_tensor, weight, padding=1)
-
-        # Утверждения для проверки корректности output_tensor
-        self.assertEqual(output_tensor.shape, (2, 4, 4, 4))  # Пример размеров выходного тензора с отступом
-        
-
-if __name__ == '__main__':
-    unittest.main()
+if __name__ == "__main__":
+    test_custom_conv2d_1()
+    test_custom_conv2d_2()
+    print("Тесты пройдены!")
